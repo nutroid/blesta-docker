6ce8b76394c185e776cb1c33433c9d3b10b36852
Sayak Brahmachari <sayak-brm@users.noreply.github.com>
2022-08-10 16:58:07 +0530

    Update entrypoint.sh

--- blesta_image/config/entrypoint.sh
+++ blesta_image/config/entrypoint.sh
@@ -4,10 +4,10 @@ DIR="/var/www/html"
 
 # If dir is empty, copy blesta application into webroot
 if [ "$(ls -A $DIR)" ]; then
-     echo "$DIR is not empty, skipping"
+    echo "$DIR is not empty, skipping"
 else
+    echo "Initializing Blesta"
     cp -R /var/www/blesta/. /var/www/html
 fi
 
-echo $@
 exec "$@"
